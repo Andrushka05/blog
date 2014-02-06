65d0c3fa541702a1776c4eb9f41be68693223660
Andrey <andrushka047@gmail.com>
2014-02-06 16:32:01 +0400

    edit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,16 @@
 class PostsController < ApplicationController
   def new
-
+    @post = Post.new
   end
 
   def create
     @post=Post.new(post_params)
 
-    @post.save
-    redirect_to @post
+    if @post.save
+      redirect_to @post
+    else
+      render 'new'
+    end
   end
 
   def show
@@ -16,6 +19,26 @@ class PostsController < ApplicationController
 
   def index
     @posts=Post.all
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post=Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+
+    redirect_to posts_path
   end
 
   private
